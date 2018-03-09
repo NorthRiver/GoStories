0cf888b613a0d1e4ff244377320097d22ec370c8
Unknown <chavck9154@yahoo.com.tw>
2018-03-09 13:17:13 +0100

    [UI] add the button of Login,Register,Recover password

--- src/JavaFxUI/LoginView.java
+++ src/JavaFxUI/LoginView.java
@@ -9,22 +9,44 @@ import javafx.scene.control.Button;
 public class LoginView extends Scene {
 
 
-
-
-
 	public LoginView(Group root, double width, double height) {
 		super(root, width, height);
 		// TODO Auto-generated constructor stub
-		Button btn = new Button();
-        btn.setLayoutX(100);
-        btn.setLayoutY(80);
-        btn.setText("Hello World");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+		Button BtnLogin = new Button();
+		Button BtnRegister = new Button();
+		Button BtnRecover = new Button();
+		
+		BtnLogin.setLayoutX(120);
+		BtnLogin.setLayoutY(150);
+		BtnRegister.setLayoutX(50);
+		BtnRegister.setLayoutY(200);
+		BtnRecover.setLayoutX(160);
+		BtnRecover.setLayoutY(200);
+		
+        
+		BtnLogin.setText("Login");
+		BtnRegister.setText("Register");
+		BtnRecover.setText("Recover"+"\n"+"Password");
+        
+        
+		BtnLogin.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
-                System.out.println("Hello World");
+                System.out.println("Login");
             }
         });
-        root.getChildren().add(btn);   
+		BtnRegister.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Register");
+            }
+        });
+		BtnRecover.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Recover password");
+            }
+        });
+        root.getChildren().add(BtnLogin);  
+        root.getChildren().add(BtnRegister);   
+        root.getChildren().add(BtnRecover);   
 	
 	}
 
