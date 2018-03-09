b2a5d7673025cf500e387905996efcfa9ee2479e
North <NorthRiver@laposte.net>
2018-03-09 12:44:55 +0100

    [SQL] FillDatabase update

--- FillDatabase.sql
+++ FillDatabase.sql
@@ -1,3 +1,3 @@
-INSERT INTO User (email, password, name) values ("java1@yopmail.com", "zeub", "Polnareff");
-INSERT INTO User (email, password, name) values ("java2@yopmail.com", "zeub", "Jotaro");
-INSERT INTO User (email, password, name) values ("java3@yopmail.com", "zeub", "Josuke");
+INSERT INTO users (email, password, name) values ("java1@yopmail.com", "zeub", "Polnareff");
+INSERT INTO users (email, password, name) values ("java2@yopmail.com", "zeub", "Jotaro");
+INSERT INTO users (email, password, name) values ("java3@yopmail.com", "zeub", "Josuke");
