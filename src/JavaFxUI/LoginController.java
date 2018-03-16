5ba1235a3d8df2fff524bb17f2db064e8bd1d072
North <NorthRiver@laposte.net>
2018-03-16 18:17:11 +0100

    Username

--- src/JavaFxUI/LoginController.java
+++ src/JavaFxUI/LoginController.java
@@ -1,14 +1,20 @@
 package JavaFxUI;
 
 import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import domain.User;
 import facades.FacadeLogin;
 
 public class LoginController {
+	@FXML 
+	private TextField usernameText;
+	@FXML 
+	private TextField pswdText;
 	public void login(ActionEvent event) {
 		FacadeLogin facade = FacadeLogin.getFacadeLogin();
 		try {
-			User moi = facade.login("josuke", "zeub");
+			User moi = facade.login(usernameText.getText(), pswdText.getText());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
