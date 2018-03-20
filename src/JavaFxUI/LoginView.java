d397d581b431ac397a4d9fc491d7ec7a7891c519
North <NorthRiver@laposte.net>
2018-03-20 18:14:14 +0100

    JAVAFX correctif

--- src/JavaFxUI/LoginView.java
+++ src/JavaFxUI/LoginView.java
@@ -1,21 +1,10 @@
 package JavaFxUI;
 
-import javafx.event.*;
 import java.io.IOException;
 import java.net.URL;
-
-import domain.User;
-import facades.FacadeLogin;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 
 public class LoginView extends Scene {
 
@@ -35,20 +24,7 @@ public class LoginView extends Scene {
 	
 	}
 
-	@FXML 
-	private TextField usernameText;
-	@FXML 
-	private TextField pswdText;
-	public void login(ActionEvent event) {
-		FacadeLogin facade = FacadeLogin.getFacadeLogin();
-		try {
-			User moi = facade.login(usernameText.getText(), pswdText.getText());
-			System.out.println(moi);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println(e);
-		}
-	}
+
 
 
 }
