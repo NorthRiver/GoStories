71c9229e73ca900e06e44baebc20ce9edecd69c3
Unknown <Hugo.lecler@laposte.net>
2018-03-26 18:51:57 +0200

    Corrected 1 last thing

--- src/UseCases/UserActions/userSearchView/SearchViewController.java
+++ src/UseCases/UserActions/userSearchView/SearchViewController.java
@@ -3,20 +3,15 @@ package UseCases.UserActions.userSearchView;
 import java.util.*;
 
 import javafx.event.ActionEvent;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 
-public class SearchViewController extends Scene {
+public class SearchViewController {
+
+    public SearchViewController() {
+    }
 
 
 
-
-    public SearchViewController(Parent root, double width, double height) {
-		super(root, width, height);
-		// TODO Auto-generated constructor stub
-	}
-
-	public void getProfile(ActionEvent event) {
+    public void getProfile(ActionEvent event) {
 
     }
 
