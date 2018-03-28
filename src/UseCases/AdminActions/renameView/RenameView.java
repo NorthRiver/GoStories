4166e793f0b58d85d32e2641a3029e1883175977
North <NorthRiver@laposte.net>
2018-03-28 16:03:26 +0200

    Structure 1

--- src/UseCases/AdminActions/renameView/RenameView.java
+++ src/UseCases/AdminActions/renameView/RenameView.java
@@ -1,10 +1,14 @@
 package UseCases.AdminActions.renameView;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.*;
 
 import domain.User;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * 
@@ -17,9 +21,17 @@ public class RenameView extends Scene{
 
 
 
-    public RenameView(Parent root, double width, double height) {
-		super(root, width, height);
-		// TODO Auto-generated constructor stub
+    public RenameView(AnchorPane root, double width, double height) {
+    	super(root, width, height);
+		URL url = getClass().getResource("LoginStructure.fxml");
+	    FXMLLoader fxmlLoader = new FXMLLoader(url);
+	    try {
+	    AnchorPane root2 = (AnchorPane) fxmlLoader.load();
+	    root.getChildren().add(root2);
+	    }
+	    catch (IOException ex) {
+	        System.err.println("Erreur au chargement: " + ex);
+	    }
 	}
 
 	/**
