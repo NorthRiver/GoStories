a576fe67411a77168933da3ed9da19d104a0cc94
North <NorthRiver@laposte.net>
2018-03-29 21:22:12 +0200

    Ban + User

--- src/UseCases/AdminActions/banMenuView/BanMenuController.java
+++ src/UseCases/AdminActions/banMenuView/BanMenuController.java
@@ -4,8 +4,13 @@ package UseCases.AdminActions.banMenuView;
 import java.util.*;
 
 import domain.User;
+import facade.FacadeBanMenu;
+import facade.FacadeRename;
+import javaFxUI.ViewManager;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  * 
@@ -17,36 +22,30 @@ import domain.User;
 
 public class BanMenuController {
 
- 
-    public BanMenuController() {
-    }
 
 
     private User currentUser;
-
-
+    @FXML
+    private Text usernameText;
+    @FXML
     public TextField numberOfDay;
 
     public void validate(ActionEvent event) {
-
-        // TODO implement here
-
+    	FacadeBanMenu facade = FacadeBanMenu.getFacade();
+    	//facade.;
+    	ViewManager.gotToManageUser();
         return;
     }
 
 
     public void cancel(ActionEvent event) {
-
-        // TODO implement here
-
-
-
+    	ViewManager.gotToManageUser();
         return;
     }
 
     public void init(User user) {
-
-        // TODO implement here
+        currentUser = user;
+        usernameText.setText(currentUser.username);
         return;
     }
 
