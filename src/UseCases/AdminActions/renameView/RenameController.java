fb49e347e29e4cc38365c5671123186d4b776aca
North <NorthRiver@laposte.net>
2018-03-29 20:55:12 +0200

    Rename Finished

--- src/UseCases/AdminActions/renameView/RenameController.java
+++ src/UseCases/AdminActions/renameView/RenameController.java
@@ -3,10 +3,13 @@ package UseCases.AdminActions.renameView;
 import java.util.*;
 
 import domain.User;
+import facade.FacadeManageUser;
+import facade.FacadeRename;
 import javaFxUI.ViewManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  * 
@@ -18,17 +21,22 @@ public class RenameController {
     
     @FXML
     public TextField newName;
+    @FXML
+    private Text usernameText;
 
 
     public void init(User user) {
         currentUser = user;
         newName.setText(currentUser.username);
+        usernameText.setText(currentUser.username);
         return;
     }
 
 
     public void validate(ActionEvent event) {
-        // TODO implement here
+    	FacadeRename facade = FacadeRename.getFacade();
+    	facade.renameUser(currentUser, newName.getText());
+    	ViewManager.gotToManageUser();
         return;
     }
 
--- src/facade/FacadeRename.java
+++ src/facade/FacadeRename.java
@@ -2,6 +2,8 @@ package facade;
 
 import java.util.*;
 
+import DAO.FactoryDAO;
+import DAO.UserDAO;
 import domain.User;
 
 /**
@@ -9,19 +11,30 @@ import domain.User;
  */
 public class FacadeRename extends AbstractFacade {
 
+	private static FacadeRename facadeSingleton;
+	private UserDAO userDao;
     /**
      * Default constructor
      */
     public FacadeRename() {
+		FactoryDAO factoryDao;
+		factoryDao = FactoryDAO.getFactory();
+		userDao = factoryDao.getUserDAO();
     }
-
+	public static FacadeRename getFacade() {
+		if( facadeSingleton == null ) {
+			facadeSingleton = new FacadeRename();	
+		}
+		return facadeSingleton;
+	}
     /**
      * @param user 
      * @param name 
      * @return
      */
     public void renameUser(User user, String name) {
-        // TODO implement here
+    	user.setUsername(name);
+    	userDao.saveUser(user);
         return;
     }
 
